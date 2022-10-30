1fed88266d0d444070a8c11be55133724e8fb816
ComboRush <69blackcop69@gmail.com>
2022-10-30 20:45:35 +0200

    adding base stuff
    made super simple http function to avoid callback hell !!

---
+++ src/http.ts
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+export class http {
+    constructor(url: string, search: string) {
+        return new Promise(resolve => {
+            axios.request({
+                method: "GET",
+                url: url,
+                params: {
+                    s: search.replace(" ", "+")
+                }
+            }).then((res) => {
+                console.log(res)
+                resolve(res)
+            })
+        })
+    }
+}
