d4c36e0ecf6f6bbde69caa7b3286f2a6c12e8a90
ComboRush <69blackcop69@gmail.com>
2022-11-03 02:02:27 +0200

    to test it if you want
    this commit just to make it plug and play ish but not really its just to test how it works under the hood

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import prompts from "prompts";
+import cliSelect from "cli-select";
 import { parse } from 'node-html-parser';
+
+const domain = "https://shahed4u.team/"
 
 class Http {
 
@@ -10,7 +13,7 @@ class Http {
     readonly pramaters: any
     
     constructor(host?: string, path?: string, headers?: any, params?: any) {
-        this.host = host ? host : "https://shahed4u.rodeo/"
+        this.host = host ? host : domain
         this.path = path ? path : ""
         this.pramaters = params ? params : {}
         this.headers = headers ? headers : {} 
@@ -57,13 +60,12 @@ class Page {
         this.page_link = link
     }
     async get_src() {
-        console.log(this.page_link)
         let http = new Http(this.page_link)
         let page_raw:any = await http.get()
         let $ = parse(page_raw.data)
         let elemets = $.querySelectorAll(".server--item")
         for (let i of elemets) {
-            http = new  Http("https://shahed4u.rodeo/wp-content/themes/Shahid4u-WP_HOME/Ajaxat/Single/Server.php", undefined, {
+            http = new  Http(`${domain}wp-content/themes/Shahid4u-WP_HOME/Ajaxat/Single/Server.php`, undefined, {
                 "accept": "*/*",
                 "accept-language": "en-US,en;q=0.9",
                 "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -107,16 +109,30 @@ class Api {
 }
 
 async function main() {
+    let results: String[] = []
+    const response = await prompts({
+        type: 'text',
+        name: 'value',
+        message: 'Search anything !',
+    });
     let api = new Api()
-    let http = new  Http("https://shahed4u.rodeo/", undefined, undefined, {
-        s: "house of the dragon" //change me :)
+    let http = new  Http(domain, undefined, undefined, {
+        s: response.value
     })
     let raw_page:any = await http.get()
     api.get_pages_number(raw_page.data)
     api.get_results(raw_page.data)
+    for (let i of api.results) {
+        results.push(i.title)
+    }
     console.clear()
-    let selected = api.results[1] //link number :) 
-    selected.get_src()
+    cliSelect({
+        values: results,
+    }).then((res) => {
+        console.clear()
+        let selected = api.results[Number(res.id)]
+        selected.get_src()
+    });
 }
 
 main()
